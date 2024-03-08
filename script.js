e5771f8bfaabe7ad0c01ee822dfa1f586c57e7ae
JamesonStansfield <159183491+JamesonStansfield@users.noreply.github.com>
2024-03-08 14:04:28 -0500

    Website Color Rework

--- script.js
+++
@@ -1,7 +0,0 @@
-function confront() {
-    let check = prompt("Are You Mr. Jamieson? (y/n)");
-    if(Response="y")
-    document.getElementById("check").innerHTML = "Welcome Mr. Jamieson";
-    if(Response="n");
-    prompt("If you are not Mr. Jamieson, get off my website! are you the teacher??? (y/n)")
-}
